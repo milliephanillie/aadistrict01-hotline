4197a5fe4177208d05a613a6cecd4dc793830d2d
Philip Rudy <me@philiparudy.com>
2026-08-06 16:35:35 -0500

    update schedule

---
+++ sms/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f53eabe9e6dd12eb335794f0be4a027d2f6097b0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f53eabe9e6dd12eb335794f0be4a027d2f6097b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.11.6',
+            'version' => '8.11.6.0',
+            'reference' => '57754efb0998086007ec9a63874fd85def5afb7f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
